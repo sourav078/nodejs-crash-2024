fbce61e9acb20e49675390172e257e74c048a61c
sourav078 <smondal172201@bscse.uiu.ac.bd>
2024-05-06 18:02:35 +0600

    office

---
+++ index.js
@@ -0,0 +1,9 @@
+// const {generateRandomNumber, celciusToFarenhite} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius To Farenheit: ${celciusToFarenhite(32)}`);
+
+import  getPosts  from "./postController.js";
+
+console.log(getPosts());
---
+++ server.js
@@ -0,0 +1,12 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => { 
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Sourav </h1>');
+
+});
+
+server.listen(PORT,() =>{
+    console.log(`Server running on port ${PORT}`);
+});
