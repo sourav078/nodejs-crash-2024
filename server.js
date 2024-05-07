7d76acbf6b8eefa66a4a925213f2f4762e92791a
sourav078 <smondal172201@bscse.uiu.ac.bd>
2024-05-08 03:02:27 +0600

    home

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 import http from 'http';
-const PORT = 8000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => { 
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1> Sourav </h1>');
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname,__filename);
+
+const server = http.createServer( async (req, res) => { 
+    try{
+        // check if get Request
+        if (req.method === 'GET'){
+
+            let filepath;
+
+            if (req.url === '/'){
+
+                filepath = path.join(__dirname, 'public', 'index.html');
+
+            } else if (req.url === '/about'){
+
+                filepath = path.join(__dirname, 'public', 'about.html');
+
+            } else {
+                throw new Error('Not Found');
+            }
+
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } 
+    
+    catch (error){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server Error');
+    }
+
+
+
+
+
 
 });
 
